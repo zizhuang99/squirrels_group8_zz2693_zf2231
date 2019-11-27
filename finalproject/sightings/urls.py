741057328e2e3723cf5edf60bc23263c8d860f8c
zi <zi.zhuang.99@gmail.com>
2019-11-27 03:03:06 +0000

    change sth in sighting

--- finalproject/sightings/urls.py
+++ finalproject/sightings/urls.py
@@ -3,6 +3,6 @@ from sightings.views import *
 
 urlpatterns=[
         path('',index, name='sightings_list'),
-        path('<str:Unique_Squirrel_Id>/',details,name='sighting_detail'),
+        path('<str:sighting>/',details,name='sighting_detail'),
         path('add/',add,name='add_sighting'),
         ]
--- finalproject/sightings/views.py
+++ finalproject/sightings/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Sightings
+from map.models import squirrels
 
 
 def index(request):
-    sightings=Sightings.objects.all()
+    sightings=squirrels.objects.all()
     context={'sightings':sightings,} 
     return render(request,"sightings/index.html",context)
 # Create your views here.
 def details(request,Unique_Squirrel_Id=None):
-    sighting=Sightings.objects.get(Unique_Squirrel_Id=Unique_Squirrel_Id)
-    context={'sighting':sighting,}
+    sighting2=squirrels.objects.get(Unique_Squirrel_Id=sighting)
+    context={'sighting':sighting2,}
     return render(request,"sightings/details.html",context)
 def add(request,Unique_Squirrel_Id=None):
     if request.method=="POST":
@@ -18,7 +18,7 @@ def add(request,Unique_Squirrel_Id=None):
         data=request.POST
         
 
-        ret=Sightings.objects.create(Unique_Squirrel_Id='123',longitude=data['longitutde'])
+        ret=squirrels.objects.create(Unique_Squirrel_Id='123',longitude=data['longitutde'])
         if ret:
             return HttpResponse('Your answer has been recorded successfully')
         else:
